109661d059cea684b977b6f0e778341f9df7d7ec
vinaz2 <vinaz2@illinois.edu>
2025-05-15 16:23:58 +0800

    Add files via upload

---
+++ test_file.py
@@ -0,0 +1,20 @@
+"""flow:
+
+1/install all the dependencies
+2/download the model file, playtrack files
+3/run the try3.py (main file)
+
+
+how to test(3 ways)
+
+1/when you input a cat image, the functions appear on the screen would be the three unique 
+to cats (collect furs, clean litter and feed the treats), vice versa. 
+
+2/there will be a drop box for you to input the image, after inputting, the image will appear 
+on the pygame interface, and a clear playtrack of dog bark or cat meowing will be there.
+
+3/when you click any of the actions to interact with the pet, the printing line on top of the 
+interface will change to "I love you, I will always be your side, my human."
+
+4/close the pygame, and when you input another image, the pygame will be a new one of this new image. 
+"""
