89db60bbc37dafffd141cc99f88f8416a5a475bc
vinaz2 <vinaz2@illinois.edu>
2025-05-19 16:49:39 +0800

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,22 +1,28 @@
 """flow:
 
-1/install all the dependencies
-2/download the model file, playtrack files
-3/run the try3.py (main file)
+1/install all the dependencies on your terminal
+2/download the model file(model-resnet50-final3.h5), playtrack files(wav.)
+3/run the try5.py (main file)
 
 
-how to test(3 ways)
-
-1/when you input a cat image, the functions appear on the screen would be the three unique 
-to cats (collect furs, clean litter and feed the treats); dogs: Feed Bone", "Walk Dog", "Hug Dog"; and 
-snakes: "Feed Rats", "Clean Ecdysis", "Curl Up Gently".
+How to test (at least 3 ways)
 
 
-2/there will be a drop box for you to input the image, after inputting, the image will appear 
-on the pygame interface, and a clear playtrack of dog bark or cat meowing will be there.
+1/There will pop up a drop box for you to input any image that has a single cat/snake/dog on it, 
+after inputting, the image will appear on the pygame interface thus starting the pygame, with a printing 
+line of "I am lonely. I need you." A clear playtrack of dog bark, cat meowing will also be there. 
+(Plus, you can also try a blurry picture of cat/dog/snake, I have tested some successfully, but 
+however not guaranteed with a accuracy as high as those with the normal images.)
+
+2/The functions appear on the screen would be the three unique to cats (collect furs, clean litter 
+and feed the treats); dogs: Feed Bone", "Walk Dog", "Hug Dog"; and snakes: "Feed Rats", "Clean Ecdysis",
+"Curl Up Gently".
 
 3/when you click any of the actions to interact with the pet, the printing line on top of the 
-interface will change to "I love you, I will always be your side, my human."
+interface will change to "I love you, I will always be your side, my human.", with the sound of pets varying 
+from before (except snake).
 
-4/close the pygame, and when you input another image, the pygame will be a new one of this new image. 
+4/close the pygame, and when you input another image, the pygame will start a new one of the new image. 
 """
+
+
