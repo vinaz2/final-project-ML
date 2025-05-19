ff7b8d8933263132ccfcdc82af11e25c11d407cc
vinaz2 <vinaz2@illinois.edu>
2025-05-19 16:41:56 +0800

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -8,7 +8,9 @@
 how to test(3 ways)
 
 1/when you input a cat image, the functions appear on the screen would be the three unique 
-to cats (collect furs, clean litter and feed the treats), vice versa. 
+to cats (collect furs, clean litter and feed the treats); dogs: Feed Bone", "Walk Dog", "Hug Dog"; and 
+snakes: "Feed Rats", "Clean Ecdysis", "Curl Up Gently".
+
 
 2/there will be a drop box for you to input the image, after inputting, the image will appear 
 on the pygame interface, and a clear playtrack of dog bark or cat meowing will be there.
